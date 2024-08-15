8ed391629d3e65a0b4d130c6de93283561a52ad4
Aniket Kangane <aniketbkangane9637@gmail.com>
2024-08-15 11:13:42 +0530

    express added

--- index.js
+++ index.js
@@ -1,37 +1,63 @@
-async function Drive() {
-  try {
-    const response = await fetch("https://drive-bnpw.onrender.com");
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
+const express = require("express");
+const cors = require("cors");
 
-async function Board() {
-  try {
-    const response = await fetch(
-      "https://board-server-rdag.onrender.com/tasks"
-    );
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-async function collabe() {
-  try {
-    const response = await fetch("https://collabe-server.onrender.com/health");
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET"],
+  })
+);
 
-setInterval(() => {
-  collabe();
-  Drive();
-  Board();
-}, 50000);
+app.get("/start", (req, resp) => {
+  async function Drive() {
+    try {
+      const response = await fetch("https://drive-bnpw.onrender.com");
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function Board() {
+    try {
+      const response = await fetch(
+        "https://board-server-rdag.onrender.com/tasks"
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function collabe() {
+    try {
+      const response = await fetch(
+        "https://collabe-server.onrender.com/health"
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  setInterval(() => {
+    collabe();
+    Drive();
+    Board();
+  }, 50000);
+
+  resp.status(200).json({
+    message: "job started",
+  });
+});
+
+app.listen(8080, () => {
+  console.log("server is running");
+});
